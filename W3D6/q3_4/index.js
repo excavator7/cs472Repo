40cfd937805b4868518b4814837124f7f1f929bf
excavator7 <54738272+excavator7@users.noreply.github.com>
2022-11-20 20:49:02 -0600

    f

--- W3D6/q3_4/index.js
+++ W3D6/q3_4/index.js
@@ -1,62 +1,57 @@
 const express = require('express');
 const app = express();
+
 const path = require('path');
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-
-app.use(express.urlencoded({extended:true}));
-
 const product = require('./module/product');
 const cart = require('./module/shoppingCart');
 
+let products = [{id: 1, name: "Mars", description: "Chocolate", price: 2, quantity: 1},
+                {id: 2, name: "Twix", description: "Chocolate", price: 3, quantity: 3},
+                {id: 3, name: "Maltesers", description: "Chocolate", price: 4, quantity: 0}];
+            
 let cartMap = new Map();
 
-let products = [new product("Mars", "Chocolate", 10, 123, 1),
-                new product("Snickers", "Chocolate", 1, 234, 2),
-                new product("Rice", "basmati", 20, 245, 0)];
+app.use(express.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render("home", {prod: products});
+    res.render("home", {products});
 });
 
 app.get('/product', (req, res) => {
-    let id = parseInt(req.query.id);
+    const id = parseInt(req.query.id);
     console.log('id: ' + id)
-    let prod = products.filter(x => x.id == id)[0];
-    res.render("product", {prod: prod});
+    const item = products.filter(x => x.id === id)[0];
+    res.render("product", {item});
 });
 
 app.post('/addToCart', (req, res) => {
-    let name = req.body.name;
-    let price = req.body.price;
-    let quantity = req.body.quantity;
-    console.log(name+', '+price+', '+quantity);
-    let cartItem = products.find(p => p.name === name && p.quantity > 0);
+    const {name, price} = req.body;
+    //let price = req.body.price;
+    //let quantity = req.body.quantity;
+    console.log(name+', '+price);
+    let item = products.find(p => p.name === name && p.quantity > 0);
 
-    if(cartItem){
+    if(item){
         if(cartMap.has(name)){
-            console.log('heeere')
             let item = cartMap.get(name);
             item.quantity +=1;
             item.price = parseInt(price);
         }else{
-            console.log('theeere')
             let item = {name, quantity: 1, price: parseInt(price)};
             cartMap.set(name, item);
         }
         item.quantity--;
-        
+        res.redirect(303, "/cart");
     }else{
         console.log('cart size: ' + cartMap.size);
-    }
-    console.log('redirect the page.....')
-    res.redirect(303, "/cart");
+    }    
 });
 
 app.get("/cart", (req, res) => {
     console.log('redirecteeeed');
-    res.render('shoppingCart', {cartItems: [...cartMap.values()]});
+    res.render('shoppingCart', {item: [...cartMap.values()]});
 });
 
 app.listen(3000);
